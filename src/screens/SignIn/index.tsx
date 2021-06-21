dbd873a8de19f97fc9943e70eaeb47af9774ff76
Marcos Vinicius <marcosvims@gmail.com>
2021-06-21 16:20:08 -0300

    primeiro dia nlw/SignIn screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { View, Text, Image, StatusBar } from "react-native"
+import { styles } from "./styles"
+import IllustrationImg from "../../assets/illustration.png"
+import { ButtonIcon } from "../../components/ButtonIcon"
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Text style={styles.title}>
+          Conecte-se{"\n"}e organize suas{"\n"}
+          jogatinas
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{"\n"}favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entre com Discord" activeOpacity={0.7} />
+      </View>
+    </View>
+  )
+}
